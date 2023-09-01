8dcb11d42445a5f6a86d6ebf6bc6657bcd0897bb
Benjamin Flanagin <bflanagin@gmail.com>
2023-09-01 12:41:03 -0500

    initial work

--- bot1.gd
+++ bot1.gd
@@ -1,7 +1,24 @@
 extends Node
 
-var states:Dictionary = {}
+@export var states:Dictionary = {}
 
+signal state_changed(state,action,target)
+var in_range:Array
+var following:bool = false
+var lock_on:Node3D
+var hit:bool = false
+var has_object:Node3D
+var ally_open:Array
+@export var targets:Dictionary = {
+	"goal":null,
+	"allys":[],
+	"enemies":[],
+	"mcguffin":null
+}
+var areas:Array
+var team:int = 0
+var search:bool = false
+var catch:bool = false
 ## The state_template is used to guide the decisions of the bot. 
 # event = the stimuli that triggers the decsion  
 # action = What the bot does when the event is triggered
@@ -21,23 +38,48 @@ var state_template:Dictionary = {
 var current_state:int = 0:
 	set(new_state):
 		current_state = new_state
-		print_debug(get_current_state(current_state)) 
+		
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var the_states = [["idle",null,null,null]]
+#		["chase",in_range,lock_on,following],
+#		["ram",in_range,lock_on,hit],
+#		["pass",has_object,ally_open,has_object],
+#		["shoot",has_object,targets["goal"],has_object],
+#		["defend",targets["mcguffin"],areas[team],targets["mcguffin"]],
+#		["search",lock_on,search,lock_on],
+#		["catch",in_range,catch,targets["mcguffin"]]
+#		]
+		
+	for s in the_states:
+		add_state(s[0],s[1],s[2],s[3])
 
+func get_current_state():
+	return states.keys()[current_state]
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-##func _process(delta):
-##	pass
-
-func _physics_process(delta):
-	pass
-
-
-func get_current_state(state):
-	return states.keys()[state]
-
-func add_state(state_name:String):
+func add_state(state_name:String,event,action,goal):
 	states[state_name] = state_template.duplicate()
+
+func compute():
+	var available_states = states.keys()
+	match current_state:
+		0: # Idle
+			pass
+		1: # chase
+			pass
+		2: # ram
+			pass
+		3: #pass
+			pass
+		4: #shoot
+			pass
+		5: #defend
+			pass
+		6: #search
+			pass
+		7: #catch
+			pass
+		8: #callout
+			pass
+			
